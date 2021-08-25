6b973eb22b87abd31d793b02f64259f754053afe
tezcansarizeybek <sarizeybektezcan@gmail.com>
2021-08-25 14:49:23 +0300

    init

--- android/src/main/java/com/tezcansarizeybek/esc_bluetooth_serial/Constant.java
+++
@@ -1,5 +0,0 @@
-package android.src.main.java.com.tezcansarizeybek.esc_bluetooth_serial;
-
-public class Constant {
-    public static final int abnormal_Disconnection = 0x011; // Abnormal disconnection
-}
--- android/src/main/java/com/tezcansarizeybek/esc_bluetooth_serial/PrinterCommand.java
+++
@@ -1,7 +0,0 @@
-package android.src.main.java.com.tezcansarizeybek.esc_bluetooth_serial;
-
-public enum PrinterCommand {
-    ESC,
-    TSC,
-    CPCL
-}
